Refactor dataset and baseline: - Optimize pointcloud mean computation - Fix visualization ray color bug - Compute ground truth unit-norm anchor orientations instead of delta vectors - Parameterize extra training objectives

dataset/base/__init__.py
@@ -15,7 +15,7 @@ transforming it may be extended through class inheritance in a specific dataset 
 
 import abc
 import pickle
-from typing import Any, List, Tuple
+from typing import Any, Dict, List, Tuple
 
 from torch.utils.data import Dataset
 
@@ -36,11 +36,14 @@ class BaseDataset(Dataset, abc.ABC):
         self._augment = augment and split == "train"
         self._scaling = scaling
         self._debug = debug
-        objects, grasps, dataset_name = self._load_objects_and_grasps(
-            tiny, split, seed=seed
-        )
+        (
+            objects,
+            objects_w_contacts,
+            grasps,
+            dataset_name,
+        ) = self._load_objects_and_grasps(tiny, split, seed=seed)
         self._sample_paths = self._load(
-            dataset_root, tiny, split, objects, grasps, dataset_name
+            dataset_root, tiny, split, objects, objects_w_contacts, grasps, dataset_name
         )
 
     @abc.abstractmethod
@@ -49,7 +52,8 @@ class BaseDataset(Dataset, abc.ABC):
         dataset_root: str,
         tiny: bool,
         split: str,
-        objects: List,
+        objects: Dict,
+        objects_w_contacts: List,
         grasps: List,
         dataset_name: str,
     ) -> List[str]:

dataset/contactpose.py
@@ -15,7 +15,7 @@ import pickle
 import random
 import sys
 from contextlib import redirect_stdout
-from typing import List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple
 
 import numpy as np
 import torch
@@ -96,7 +96,7 @@ class ContactPoseDataset(BaseDataset):
 
     def _load_objects_and_grasps(
         self, tiny: bool, split: str, seed: int = 0
-    ) -> Tuple[list, list, str]:
+    ) -> Tuple[dict, list, list, str]:
         if not osp.isdir(osp.join(get_original_cwd(), "vendor", "ContactPose")):
             raise RuntimeError(
                 "Please clone the ContactPose dataset in the vendor/ directory as 'ContactPose'"
@@ -170,8 +170,10 @@ class ContactPoseDataset(BaseDataset):
         else:
             object_names = object_names[-self._validation_objects :]
         # Now, build the dataset.
-        objects = []
-        grasps = []
+        # objects: unique object paths
+        # objects_w_contacts: object with contacts paths
+        # grasps: MANO parameters w/ global pose pickle paths
+        objects, objects_w_contacts, grasps = {}, [], []
         print(f"[*] Loading ContactPose{' (tiny)' if tiny else ''}...")
 
         dataset_path = osp.join(
@@ -182,11 +184,8 @@ class ContactPoseDataset(BaseDataset):
             + f"{'right-hand' if self._right_hand_only else 'both-hands'}_seed-{seed}.pkl",
         )
         if osp.isfile(dataset_path):
-            objects, grasps = pickle.load(open(dataset_path, "rb"))
+            objects, objects_w_contacts, grasps = pickle.load(open(dataset_path, "rb"))
         else:
-            if not osp.isfile(osp.join(self._cache_dir, "missing.txt")):
-                with open(osp.join(self._cache_dir, "missing.txt"), "w") as f:
-                    f.write("")
             for obj_name in tqdm(object_names):
                 for p_num, intent, hand in cp_dataset[obj_name]:
                     # Generate the data since the dataset file does not exist.
@@ -199,7 +198,7 @@ class ContactPoseDataset(BaseDataset):
                     )
                     if data is None:
                         continue
-                    obj_mesh_path, grasp = data
+                    obj_mesh_w_contacts_path, grasp = data
                     with open(grasp_path, "wb") as f:
                         # Log participant no and intent in the data so we can debug later on.
                         pickle.dump(
@@ -212,9 +211,11 @@ class ContactPoseDataset(BaseDataset):
                             },
                             f,
                         )
-                    objects.append(obj_mesh_path)
+                    objects_w_contacts.append(obj_mesh_w_contacts_path)
                     grasps.append(grasp_path)
-            pickle.dump((objects, grasps), open(dataset_path, "wb"))
+                    if obj_name not in objects:
+                        objects[obj_name] = obj_mesh_w_contacts_path
+            pickle.dump((objects, objects_w_contacts, grasps), open(dataset_path, "wb"))
         print(
             f"[*] Loaded {len(object_names)} objects and {len(grasps)} grasp sequences"
         )
@@ -226,15 +227,21 @@ class ContactPoseDataset(BaseDataset):
                 ],
             )
         )
-        assert len(objects) == len(grasps)
-        return objects, grasps, osp.basename(dataset_path.split(".")[0])
+        assert len(objects_w_contacts) == len(grasps)
+        return (
+            objects,
+            objects_w_contacts,
+            grasps,
+            osp.basename(dataset_path.split(".")[0]),
+        )
 
     def _load(
         self,
         dataset_root: str,
         tiny: bool,
         split: str,
-        objects: List[str],
+        objects: Dict[str, str],
+        objects_w_contacts: List[str],
         grasps: List,
         dataset_name: str,
     ) -> List[str]:
@@ -270,8 +277,9 @@ class ContactPoseDataset(BaseDataset):
                 pointclouds = pickle.load(f)
         else:
             print("[*] Computing object pointclouds...")
-            for object_with_contacts_pth in tqdm(objects):
-                obj_mesh = o3dio.read_triangle_mesh(object_with_contacts_pth)
+            # This is very expensive in memory so we can batch it.
+            for object_pth in tqdm(objects.values()):
+                obj_mesh = o3dio.read_triangle_mesh(object_pth)
                 obj_ptcld = torch.from_numpy(
                     np.asarray(
                         obj_mesh.sample_points_uniformly(self._obj_ptcld_size).points  # type: ignore
@@ -288,8 +296,8 @@ class ContactPoseDataset(BaseDataset):
                 pickle.dump(pointclouds, f)
         # For each object-grasp pair, compute the CHOIR field.
         print("[*] Computing CHOIR fields...")
-        for object_ptcld, mesh_pth, grasp_pth in tqdm(
-            zip(pointclouds, objects, grasps), total=len(objects)
+        for mesh_pth, grasp_pth in tqdm(
+            zip(objects_w_contacts, grasps), total=len(objects_w_contacts)
         ):
             # Load the object mesh and MANO params
             with open(grasp_pth, "rb") as f:
@@ -339,6 +347,12 @@ class ContactPoseDataset(BaseDataset):
                     raise NotImplementedError
 
                 # Compute the CHOIR field
+                obj_mesh = o3dio.read_triangle_mesh(mesh_pth)
+                obj_ptcld = torch.from_numpy(
+                    np.asarray(
+                        obj_mesh.sample_points_uniformly(self._obj_ptcld_size).points  # type: ignore
+                    )
+                ).cpu()
                 visualize = self._debug and (random.random() < 0.1)
                 has_visualized = False
                 for i in range(self._n_random_choir_per_sample):
@@ -350,11 +364,14 @@ class ContactPoseDataset(BaseDataset):
                         continue
                     anchors = anchor_layer(verts)
                     choir, pcl_mean, pcl_scalar, ref_pts, anchor_deltas = compute_choir(
-                        to_cuda_(object_ptcld),
+                        to_cuda_(obj_ptcld),
                         to_cuda_(anchors),
                         pointclouds_mean=to_cuda_(pointclouds_mean),
                         bps_dim=self._bps_dim,  # type: ignore
                         anchor_assignment=self._anchor_assignment,
+                    )
+                    anchor_orientations = torch.nn.functional.normalize(
+                        anchor_deltas, dim=1
                     )
                     # Compute the dense MANO contact map
                     # hand_contacts = compute_hand_contacts_simple(
@@ -367,7 +384,7 @@ class ContactPoseDataset(BaseDataset):
                     pcl_scalar = pcl_scalar.squeeze(0).cpu()
                     # hand_contacts = hand_contacts.squeeze(0).cpu()
                     joints = joints.squeeze(0).cpu()
-                    anchor_deltas = anchor_deltas.squeeze(0).cpu()
+                    anchor_orientations = anchor_orientations.squeeze(0).cpu()
                     anchors = anchors.squeeze(0).cpu()
                     rot_6d = rot_6d.squeeze(0).cpu()
                     trans = trans.squeeze(0).cpu()
@@ -382,7 +399,7 @@ class ContactPoseDataset(BaseDataset):
                     label = (
                         choir,
                         # hand_contacts,
-                        anchor_deltas,
+                        anchor_orientations,
                         joints,
                         anchors,
                         torch.tensor(mano_params["pose"]).cpu(),
@@ -412,9 +429,9 @@ class ContactPoseDataset(BaseDataset):
                             hand_contacts,
                             verts,
                             anchors,
-                            anchor_deltas,
+                            anchor_orientations,
                             mesh,
-                            object_ptcld,
+                            obj_ptcld,
                             ref_pts,
                             affine_mano,
                             self._anchor_assignment,

model/baseline.py
@@ -22,6 +22,8 @@ class BaselineModel(torch.nn.Module):
         encoder_layer_dims: Tuple[int],
         decoder_layer_dims: Tuple[int],
         latent_dim: int,
+        predict_anchor_orientation: bool,
+        predict_mano: bool,
     ) -> None:
         super().__init__()
         self.single_anchor = anchor_assignment in ["closest", "random", "batched_fixed"]
@@ -56,29 +58,27 @@ class BaselineModel(torch.nn.Module):
             decoder.append(torch.nn.BatchNorm1d(decoder_layer_dims[i + 1]))
             decoder.append(torch.nn.ReLU())
         self.decoder = torch.nn.Sequential(*decoder)
-        anchor_orientation_decoder: List[torch.nn.Module] = [
-            torch.nn.Linear(latent_dim, decoder_layer_dims[0]),
-            torch.nn.BatchNorm1d(decoder_layer_dims[0]),
-            torch.nn.ReLU(),
-        ]
-        for i in range(len(decoder_layer_dims) - 1):
+        self.anchor_orientation_decoder = None
+        if predict_anchor_orientation:
+            anchor_orientation_decoder: List[torch.nn.Module] = [
+                torch.nn.Linear(latent_dim, decoder_layer_dims[0]),
+                torch.nn.BatchNorm1d(decoder_layer_dims[0]),
+                torch.nn.ReLU(),
+            ]
+            for i in range(len(decoder_layer_dims) - 1):
+                anchor_orientation_decoder.append(
+                    torch.nn.Linear(decoder_layer_dims[i], decoder_layer_dims[i + 1])
+                )
+                anchor_orientation_decoder.append(
+                    torch.nn.BatchNorm1d(decoder_layer_dims[i + 1])
+                )
+                anchor_orientation_decoder.append(torch.nn.ReLU())
             anchor_orientation_decoder.append(
-                torch.nn.Linear(decoder_layer_dims[i], decoder_layer_dims[i + 1])
+                torch.nn.Linear(decoder_layer_dims[-1], 3 * bps_dim)
             )
-            anchor_orientation_decoder.append(
-                torch.nn.BatchNorm1d(decoder_layer_dims[i + 1])
+            self.anchor_orientation_decoder = torch.nn.Sequential(
+                *anchor_orientation_decoder
             )
-            anchor_orientation_decoder.append(torch.nn.ReLU())
-        anchor_orientation_decoder.append(
-            torch.nn.Linear(decoder_layer_dims[-1], 3 * bps_dim)
-        )
-        self.anchor_orientation_decoder = torch.nn.Sequential(
-            *anchor_orientation_decoder
-        )
-        # self._bps_dist_decoder = torch.nn.Sequential(
-        # torch.nn.Linear(decoder_layer_dims[-1], bps_dim), torch.nn.ReLU()
-        # )
-        # self._bps_deltas_decoder = torch.nn.Linear(decoder_layer_dims[-1], bps_dim * 3)
         self._anchor_dist_decoder = torch.nn.Sequential(
             torch.nn.Linear(
                 decoder_layer_dims[-1], bps_dim * (1 if self.single_anchor else 2)
@@ -101,11 +101,13 @@ class BaselineModel(torch.nn.Module):
         B, P, _ = input_shape
         _x = x
         latent = self.encoder(x.flatten(start_dim=1))
-        anchor_orientations = self.anchor_orientation_decoder(latent).view(B, P, 3)
-        anchor_orientations = torch.nn.functional.normalize(anchor_orientations, dim=-1)
+        anchor_orientations = None
+        if self.anchor_orientation_decoder is not None:
+            anchor_orientations = self.anchor_orientation_decoder(latent).view(B, P, 3)
+            anchor_orientations = torch.nn.functional.normalize(
+                anchor_orientations, dim=-1
+            )
         x = self.decoder(latent)
-        # bps_dist = self._bps_dist_decoder(x).view(B, P, 1)  # N, 1
-        # bps_deltas = self._bps_deltas_decoder(x).view(B, P, 3)  # N, 3
         sqrt_distances = self._anchor_dist_decoder(x)
         anchor_dist = (sqrt_distances**2).view(
             B, P, 1 if self.single_anchor else 2
@@ -143,6 +145,8 @@ class BaselineUNetModel(torch.nn.Module):
         encoder_layer_dims: Tuple[int],
         decoder_layer_dims: Tuple[int],
         latent_dim: int,
+        predict_anchor_orientation: bool,
+        predict_mano: bool,
     ) -> None:
         super().__init__()
         self.single_anchor = anchor_assignment in ["closest", "random", "batched_fixed"]

src/base_trainer.py
@@ -15,12 +15,12 @@ from typing import Dict, List, Optional, Tuple, Union
 
 import plotext as plt
 import torch
+import wandb
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 from torchmetrics import MeanMetric
 from tqdm import tqdm
 
-import wandb
 from conf import project as project_conf
 from src.losses.hoi import CHOIRLoss, DualHOILoss
 from utils import blink_pbar, to_cuda, update_pbar_str
@@ -96,7 +96,7 @@ class BaseTrainer:
         noisy_choir, pcl_mean, pcl_scalar, bps_dim = x
         (
             choir_gt,
-            anchor_deltas,
+            anchor_orientations,
             joints_gt,
             anchors_gt,
             pose_gt,
@@ -107,8 +107,9 @@ class BaseTrainer:
         y_hat = self._model(noisy_choir)
         loss = self._training_loss(
             y, y_hat
-        )  # {'distances': _, 'orientations': _, 'anchors': _}
-        return loss["distances"] + 3 * loss["anchor_deltas"], loss
+        )  # {'distances': _, 'orientations': _, 'anchors': _, 'mano': _}
+        # TODO: Compute final loss based on components returned (not all may be used)
+        return loss["distances"], loss  # + 3 * loss["anchor_deltas"], loss
 
     def _train_epoch(
         self, description: str, visualize: bool, epoch: int, last_val_loss: float

src/losses/hoi.py
@@ -19,11 +19,19 @@ from utils.dataset import compute_hand_contacts_simple
 
 
 class CHOIRLoss(torch.nn.Module):
-    def __init__(self, anchor_assignment: str) -> None:
+    def __init__(
+        self,
+        anchor_assignment: str,
+        predict_anchor_orientation: bool,
+        predict_mano: bool,
+    ) -> None:
         super().__init__()
         self._mse = torch.nn.MSELoss()
+        self._cosine_similarity = torch.nn.CosineSimilarity(dim=2)
         self._cross_entropy = torch.nn.CrossEntropyLoss()
         self._anchor_assignment = anchor_assignment
+        self._predict_anchor_orientation = predict_anchor_orientation
+        self._predict_mano = predict_mano
 
     def forward(
         self,
@@ -36,7 +44,7 @@ class CHOIRLoss(torch.nn.Module):
             )
         (
             choir_gt,
-            anchor_deltas,
+            anchor_orientations,
             joints_gt,
             anchors_gt,
             pose_gt,
@@ -45,12 +53,14 @@ class CHOIRLoss(torch.nn.Module):
             trans_gt,
         ) = y
         choir_pred, orientations_pred = y_hat["choir"], y_hat["orientations"]
-        choir_gt, target_anchor_deltas = choir_gt, anchor_deltas
-        pred_anchor_deltas = orientations_pred * choir_pred[:, :, 4].unsqueeze(-1)
+        choir_gt, target_anchor_orientations = choir_gt, anchor_orientations
         loss = {
             "distances": self._mse(choir_gt[:, :, 4], choir_pred[:, :, 4]) * 1000,
-            "anchor_deltas": self._mse(target_anchor_deltas, pred_anchor_deltas) * 1000,
         }
+        if self._predict_anchor_orientation:
+            loss["orientations"] = self._cosine_similarity(
+                orientations_pred, target_anchor_orientations
+            )
         if self._anchor_assignment == "closest":
             loss["anchors"] = self._cross_entropy(y[:, :, -32:], y_hat[:, :, -32:])
         return loss

utils/visualization.py
@@ -47,7 +47,7 @@ def visualize_model_predictions(
             pcl_scalar,
         )
         pred = model(noisy_choir)
-        choir_pred, orientations_pred = pred["choir"], pred["orientations"]
+        choir_pred = pred["choir"]
         mano_params_gt = {
             "pose": pose_gt,
             "beta": beta_gt,
@@ -257,7 +257,7 @@ def visualize_CHOIR(
     dense_contacts: torch.Tensor,
     verts: torch.Tensor,
     anchors: torch.Tensor,
-    anchor_deltas: torch.Tensor,
+    anchor_orientations: torch.Tensor,
     obj_mesh,
     obj_pointcloud,
     reference_obj_points: torch.Tensor,
@@ -326,15 +326,26 @@ def visualize_CHOIR(
         # It is obtained from min-max normalization of the distance in the choir field,
         # without known range. The color range is 0 to 16777215.
         color = int(
-            (choir[i, 0] - choir[:, 0].min())
-            / (choir[:, 0].max() - choir[:, 0].min())
+            (choir[i, 4] - choir[:, 4].min())
+            / (choir[:, 4].max() - choir[:, 4].min())
             * 16777215
+        )
+        # This is to check that the delta vectors are correct: (should be the same visual
+        # result as above)
+        assert np.allclose(
+            np.array([reference_obj_points[i, :].cpu().numpy(), anchor.cpu().numpy()]),
+            np.array(
+                [
+                    reference_obj_points[i, :].cpu().numpy(),
+                    (
+                        reference_obj_points[i, :].cpu()
+                        + (choir[i, 4] * anchor_orientations[i, :])
+                    ).numpy(),
+                ]
+            ),
         )
         pl.add_lines(
             np.array([reference_obj_points[i, :].cpu().numpy(), anchor.cpu().numpy()]),
-            # This is to check that the delta vectors are correct: (should be the same visual
-            # result as above)
-            # np.array([reference_obj_points[i, :].cpu().numpy(), (reference_obj_points[i, :].cpu() + anchor_deltas[i, :]).numpy()]),
             width=1,
             color="#" + hex(color)[2:].zfill(6),
             name=f"ray{i}",