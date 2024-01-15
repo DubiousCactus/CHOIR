52a0124b6c81b4b5a0af28f24a386d6401c2a1de
Theo <theo.morales.fr@gmail.com>
2024-01-15 18:24:19 +0000

    Bring back the plateau sched.

--- conf/experiment.py
+++ conf/experiment.py
@@ -353,7 +353,7 @@ Experiment = builds(
         {"dataset": "contactpose"},
         {"model": "baseline"},
         {"optimizer": "adam"},
-        {"scheduler": "exp"},
+        {"scheduler": "plateau"},
         {"run": "default"},
         {"training_loss": "choir"},
     ],
