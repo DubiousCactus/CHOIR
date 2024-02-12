dbada40be8c571da4709ddcdc48f84a02476590c
Theo <theo.morales.fr@gmail.com>
2024-02-12 19:28:24 +0000

    Add missing globals.py

---
+++ globals.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyright © 2024 Théo Morales <theo.morales.fr@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Global variables.
+"""
+
+
+FLAT_LOWER_INDICES = [
+    0,
+    3,
+    4,
+    6,
+    7,
+    8,
+]  # Indices of the flattened lower triangular matrix
