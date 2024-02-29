Fix covariance prediction pre-procesing

dataset/oakink.py
@@ -102,6 +102,14 @@ class OakInkDataset(BaseDataset):
         ]
     )
 
+    # TODO: Those are from ContactOpt
+    contacts_min = torch.tensor(
+        [-0.0196, -0.0189, -0.0192, 0.0000, -0.0106, 0.0000, -0.0110, -0.0108, 0.0000]
+    )
+    contacts_max = torch.tensor(
+        [0.0196, 0.0193, 0.0200, 0.0133, 0.0113, 0.0118, 0.0105, 0.0110, 0.0110]
+    )
+
     def __init__(
         self,
         dataset_root: str,
@@ -192,7 +200,7 @@ class OakInkDataset(BaseDataset):
     ) -> Tuple[dict, list, list, str]:
         idx = 0
         if tiny:
-            n_samples = 1000 if split == "train" else 100
+            n_samples = 10000 if split == "train" else 2000
         else:
             os.environ["OAKINK_DIR"] = self._dataset_root
             from oikit.oi_shape import OakInkShape
@@ -201,7 +209,7 @@ class OakInkDataset(BaseDataset):
                 data_split=split, mano_assets_root="vendor/manotorch/assets/mano"
             )
             n_samples = (
-                len(dataset) if not tiny else (1000 if split == "train" else 100)
+                len(dataset) if not tiny else (10000 if split == "train" else 2000)
             )
         dataset_path = osp.join(
             self._cache_dir,
@@ -218,6 +226,18 @@ class OakInkDataset(BaseDataset):
             with open(dataset_path, "rb") as f:
                 compressed_pkl = f.read()
                 objects, grasps = pickle.loads(blosc2.decompress(compressed_pkl))
+            # new_objects, new_grasps = [], []
+            # for obj_path, grasp_path in zip(objects, grasps):
+            # obj_path = obj_path.replace("/media/data2/moralest/", "/Users/cactus/Code/")
+            # grasp_path = grasp_path.replace(
+            # "/media/data2/moralest/", "/Users/cactus/Code/"
+            # )
+            # new_objects.append(obj_path)
+            # new_grasps.append(grasp_path)
+            # with open(dataset_path, "wb") as f:
+            # pkl = pickle.dumps((new_objects, new_grasps))
+            # compressed_pkl = blosc2.compress(pkl)
+            # f.write(compressed_pkl)
         else:
             os.environ["OAKINK_DIR"] = self._dataset_root
             from oikit.oi_shape import OakInkShape

src/multiview_tester.py
@@ -333,7 +333,6 @@ class MultiViewTester(MultiViewTrainer):
         cached_obj_path = "batch_obj_data.pkl"
         if os.path.exists(cached_obj_path):
             with open(cached_obj_path, "rb") as f:
-                # batch_obj_data = pickle.load(f, map_location="cpu")
                 batch_obj_data = to_cuda_(
                     torch.load(f, map_location=torch.device("cpu"))
                 )
@@ -456,7 +455,7 @@ class MultiViewTester(MultiViewTrainer):
                         batch_obj_data,
                     )
                 else:
-                    sample_to_viz = 2
+                    sample_to_viz = 47
                     contacts_pred, obj_points, obj_normals = (
                         None,
                         None,
@@ -546,6 +545,17 @@ class MultiViewTester(MultiViewTrainer):
                         print(
                             f"GT gaussian params shape: {gt_contact_gaussian_params[sample_to_viz, -1].shape}"
                         )
+                        print("-------- Gaussian variances --------")
+                        for i in range(32):
+                            cov = (
+                                gt_contact_gaussian_params[sample_to_viz, -1][i, 3:]
+                                .cpu()
+                                .view(-1, 3, 3)[0]
+                            )
+                            var = torch.take(cov, torch.tensor([0, 4, 8]))
+                            print(
+                                f"Anchor {i+1}/32: var={var}, cov_norm={torch.norm(cov)}, var_norm={torch.norm(var)}"
+                            )
                         visualize_3D_gaussians_on_hand_mesh(
                             gt_hand_mesh,
                             batch_obj_data["mesh"][sample_to_viz],
@@ -558,7 +568,7 @@ class MultiViewTester(MultiViewTrainer):
                             gt_contact_gaussian_params[sample_to_viz, -1].cpu(),
                             gt_anchors[sample_to_viz].cpu(),
                         )
-
+                        print("--------------------")
                         print("====== Reconstructing contacts from 3D Gaussians ======")
                         pred_contact_gaussian_params = torch.cat(
                             (
@@ -569,6 +579,22 @@ class MultiViewTester(MultiViewTrainer):
                             ),
                             dim=-1,
                         )
+                        print("-------- Gaussian variances --------")
+                        for i in range(32):
+                            cov = (
+                                pred_contact_gaussian_params[i, 3:]
+                                .cpu()
+                                .view(-1, 3, 3)[0]
+                            )
+                            var = torch.take(cov, torch.tensor([0, 4, 8]))
+                            print(
+                                f"Anchor {i+1}/32: var={var}, cov_norm={torch.norm(cov)}, var_norm={torch.norm(var)}"
+                            )
+                            if torch.norm(cov) < 1e-6:
+                                pred_contact_gaussian_params[i] = torch.zeros_like(
+                                    pred_contact_gaussian_params[i]
+                                ).to(pred_contact_gaussian_params.device)
+                        print("--------------------")
                         visualize_3D_gaussians_on_hand_mesh(
                             pred_hand_mesh,
                             batch_obj_data["mesh"][sample_to_viz],

utils/training.py
@@ -104,9 +104,10 @@ def optimize_pose_pca_from_choir(
                 rot = rot.mean(dim=1)
             if len(trans.shape) > 2:
                 trans = trans.mean(dim=1)
-        assert (
-            theta.shape[0] == B
-        ), "compute_pca_from_choir(): batch size mismatch between initial parameters and CHOIR field."
+        assert theta.shape[0] == B, (
+            "compute_pca_from_choir(): batch size mismatch between initial parameters and CHOIR field."
+            + f" theta.shape: {theta.shape}, B: {B}"
+        )
     rot, trans, theta, beta = to_cuda_((rot, trans, theta, beta))
     for p in (rot, trans, theta, beta):
         p.requires_grad = True

utils/visualization.py
@@ -1179,15 +1179,21 @@ def visualize_hand_contacts_from_3D_gaussians(
         anchor_distances, 1, dim=-1, largest=False, sorted=False
     )
     for i in range(gaussian_params.shape[0]):
-        if torch.all(gaussian_params[i] == 0):
+        if torch.allclose(
+            gaussian_params[i], torch.zeros_like(gaussian_params[i]), atol=1e-6
+        ):
             continue
         mean = gaussian_params[i, :3] + anchors[i]
-        covariance = gaussian_params[i, 3:].reshape(3, 3)
+        covariance = (
+            gaussian_params[i, 3:].reshape(3, 3) + 1e-9 * torch.eye(3)[None, ...]
+        )
         gaussian = torch.distributions.MultivariateNormal(mean, covariance)
         this_anchor_indices = (anchor_indices == i).squeeze(-1)
         anchor_verts = hand_verts[this_anchor_indices]
         # vertex_contacts[this_anchor_indices] = torch.clamp_max(torch.exp(gaussian.log_prob(anchor_verts)), 1.0)
         contact_vals = torch.exp(gaussian.log_prob(anchor_verts))
+        if contact_vals.max() == 0:
+            continue
         print(f"Max contact values for Gaussian {i}/32: {contact_vals.max()}")
         vertex_contacts[this_anchor_indices] = contact_vals / contact_vals.max()
 